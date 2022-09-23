7f780c75e8e4dec0fc3d6bebec8564106fbad6db
zhuicode99 <enlin291814833@hotmail.com>
2022-09-23 00:06:10 +0000

    update code by parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -44,7 +44,7 @@ Elliot Dickinson has an id of 4 and was in the FEB12 cohort
 Lloyd Boehm has an id of 5 and was in the FEB12 cohort */ 
 
 
-pool.query(`
+/* pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
@@ -55,4 +55,28 @@ LIMIT ${process.argv[3] || 5};
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack)); 
+
+*///before parameterize
+
+
+//Parameterized Query to prevent malicious injection
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack)); 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+/* pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
@@ -27,3 +27,26 @@ ORDER BY teachers.name;
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 }); */
+
+
+//Parameterized Query to prevent malicious injection
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teachers.name;
+`
+
+const cohortName = process.argv[2] || 'JUL02'
+const values = [cohortName]
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(something => {
+    console.log(`${something.cohort} : ${something.teacher}`); //something can be anything
+  })
+}).catch(err => console.error('query error', err.stack));
